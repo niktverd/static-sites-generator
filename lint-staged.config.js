f10cb795e89988a348f9273e18a8a01a428471a2
Nik Tverd <niktverd@yandex-team.ru>
2022-07-22 02:48:25 +0600

    Initial commit

---
+++ lint-staged.config.js
@@ -0,0 +1,15 @@
+// lint-staged.config.js
+module.exports = {
+    // Type check TypeScript files
+    '**/*.(ts|tsx)': () => 'yarn tsc --noEmit',
+
+    // Lint then format TypeScript and JavaScript files
+    '**/*.(ts|tsx|js)': (filenames) => [
+        `npm run eslint --fix ${filenames.join(' ')}`,
+        `npm run prettier --write ${filenames.join(' ')}`,
+    ],
+
+    // Format MarkDown and JSON
+    '**/*.(md|json)': (filenames) =>
+        `npm run prettier --write ${filenames.join(' ')}`,
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,12 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+
+export const APP_VERSION = "v1.0.0";
+
+
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    return <Component {...pageProps} />;
 }
 
 export default MyApp
